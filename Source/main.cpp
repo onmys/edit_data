0d86895dbea290418ba390b0e568abe8da787662
onmys <shape.a.wonder@gmail.com>
2021-01-24 23:39:11 +0900

    Huffman実装

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,7 @@
 #include <sys/stat.h>
 #include "PNG/PNG.h"
 #include "BMP/BMP.h"
+#include "Huffman/Huffman.h"
 #include "LZSS/LZSS.h"
 
 int main( int argc, char* argv[] )
@@ -10,13 +11,13 @@ int main( int argc, char* argv[] )
 	_png.write( "Assets/output.png" );
 
 	auto a = _png.getChunkData( png::ChunkType::IHDR );
-	int width = *( int* )a->getDataElement( ( int )png::Ihdr::DataType::WIDTH );
+	auto width = *( int* )a->getDataElement( ( int )png::Ihdr::DataType::WIDTH );
 
 	bmp::BMP _bmp;
 	_bmp.load( "Assets/input.bmp" );
 	_bmp.write( "Assets/output.bmp" );
 
-	//-------------------------------------------------------------------
+	//	PNGで使われている圧縮アルゴリズム（LZSS、Huffman）の実装
 
 	//	ファイルサイズ取得
 	int fileSize = 0;
@@ -39,27 +40,33 @@ int main( int argc, char* argv[] )
 	fclose( fp1 );
 
 	std::vector<BYTE> rawData( data, data + fileSize );
-	std::vector<BYTE> compressedData;
-	compressedData = {};
+	std::vector<BYTE> compressedLZSSData;
+	compressedLZSSData.clear();
+	std::vector<BYTE> compressedHuffmanData;
+	compressedHuffmanData.clear();
 
 	LZSS lzss;
+	Huffman huffman;
 
 	//	データ圧縮
-	lzss.encode( rawData, compressedData );
+	lzss.encode( rawData, compressedLZSSData );
+	huffman.encode( compressedLZSSData, compressedHuffmanData );
 
-	BYTE* output = compressedData.data();
+	BYTE* output = compressedHuffmanData.data();
 
 	FILE* fp2 = nullptr;
 	if( fopen_s( &fp2, "Assets/compressedData.txt", "wb" ) != 0 )
 	{
 		return 0;
 	}
-	fwrite( output, compressedData.size(), 1, fp2 );
+	fwrite( output, compressedHuffmanData.size(), 1, fp2 );
 	fclose( fp2 );
+
+	//-------------------------------------------------------------------
 
 	//	データ解凍
 	rawData.clear();
-	lzss.decode( compressedData, rawData );
+	lzss.decode( compressedLZSSData, rawData );
 
 	output = rawData.data();
 
@@ -71,48 +78,26 @@ int main( int argc, char* argv[] )
 	fwrite( output, rawData.size(), 1, fp3 );
 	fclose( fp3 );
 
-	int compressedNum = 0;
-	int rawNum = 0;
-	for( int i = 0; i < compressedData.size(); i+=2 )
-	{
-		if( compressedData[i] == 0x01 )
-		{
-			compressedNum++;
-		}
-		else
-		{
-			rawNum++;
-		}
-	}
-	printf( "圧縮された数 : %d", compressedNum );
-	printf( "圧縮されなかった数 : %d", rawNum );
+	//int compressedNum = 0;
+	//int rawNum = 0;
+	//for( int i = 0; i < compressedData.size(); i+=2 )
+	//{
+	//	if( compressedData[i] == 0x01 )
+	//	{
+	//		compressedNum++;
+	//	}
+	//	else
+	//	{
+	//		rawNum++;
+	//	}
+	//}
+	//printf( "圧縮された数 : %d", compressedNum );
+	//printf( "圧縮されなかった数 : %d", rawNum );
 
 	//-------------------------------------------------------------------
 	
 	return 0;
 }
-
-/*
-// グレースケールに変換
-	for( int j = 0; j < bitmap.height; j++ )
-	{
-		for( i = 0; i < bitmap.width; i++ )
-		{
-			sum = 0;
-			for( int c = 0; c < bitmap.ch; c++ )
-			{
-				sum += bitmap.data[bitmap.ch * ( i + j * bitmap.width ) + c];
-			}
-			
-			ave = sum / bitmap.ch;
-		
-			for( int c = 0; c < bitmap.ch; c++ )
-			{
-				bitmap.data[bitmap.ch * (i + j * bitmap.width) + c] = ave;
-			}
-		}
-	}
-*/
 
 /*
 //	エンディアンの確認
